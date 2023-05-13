27453aed60ccd6059886463a65e8208526dc053a
devchon2 <121806095+devchon2@users.noreply.github.com>
2023-05-13 11:36:17 +0200

    first commit

---
+++ scripts/pages/index.js
@@ -0,0 +1,28 @@
+import { photographerFactory } from "../factories/photographerFactory.js";
+import { get_Datas } from "../utils/get_datas.js";
+
+
+
+// Affichage des elements de la page
+function displayData(datas) {
+  const photographersSection = document.querySelector(".photographer_section");
+
+  datas.forEach((photographer) => {
+    const photographerModel = photographerFactory(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+async function init() {
+  const { photographers } = await get_Datas(); // Récupère les photographes
+
+  // Si vous êtes sur la page index.html, affichez les photographes
+  if (document.querySelector(".photographer_section")) {
+    displayData(photographers);
+  }
+}
+
+init();
+
+
