ecdc44e3bbfe1f549ede5118b75cde637b9930cb
rachon34 <rachon3441@gmail.com>
2023-05-13 14:10:01 +0200

    test ./

--- scripts/utils/get_datas.js
+++ scripts/utils/get_datas.js
@@ -1,6 +1,6 @@
 async function get_Datas() {
    
-    let response = await fetch("/data/photographers.json");
+    let response = await fetch("./data/photographers.json");
     // et bien retourner le tableau photographers seulement une fois récupéré
     const Datas = await response.json();
     return Datas;
